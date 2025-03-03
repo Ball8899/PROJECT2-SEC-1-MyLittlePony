43ac433ea79e67e0255053cd8d45fee7320bb8c9
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-03-03 14:07:35 +0700

    [027] UPDATE update project template

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <script setup>
-   import Navigation from './components/Navigation.vue'
-import Header from './components/Header.vue'
-  
+import Navigation from "./components/Navigation.vue";
+import Header from "./components/Header.vue";
 </script>
 
-
 <template>
- <div>
-   <Navigation></Navigation>
-   <Header></Header>
-
-
- </div>
-
-
+  <div>
+    <div class="bg-cover bg-opacity bg-[100%_85%] bg-[url('https://wallpapercave.com/wp/wp10535399.jpg')]">
+      <Navigation></Navigation>
+      <Header></Header>
+    </div>
+  </div>
 </template>
-
 
 <style scoped>
 
-
 @media (min-width: 1024px) {
-
-
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,11 +4,11 @@
 
 
 <template>
- <div class="bg-amber-300/70">
+ <div class="bg-black/40">
    <div
-     class="flex text-black py-5 items-center px-10 justify-between"
+     class="flex text-white py-5 items-center px-10 justify-between"
    >
-     <h1 class="text-4xl font-bold">🐶 Just Goo</h1>
+     <h1 class="text-4xl font-bold">JET</h1><h1>.</h1><h1>GO</h1>
      <div class="relative">
        <input
          type="text"
@@ -16,7 +16,7 @@
          class="ml-5 pl-10 pr-4 py-2 bg-white rounded-sm focus:outline-none w-64"
        />
        <svg
-         class="absolute left-8 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500"
+         class="absolute left-8 top-1/2 transform -translate-y-1/2 w-5 h-5 text-white"
          xmlns="http://www.w3.org/2000/svg"
          fill="none"
          viewBox="0 0 24 24"
@@ -34,13 +34,15 @@
        <p class="py-1.5 px-3 rounded-sm">Customer Support</p>
        <p class="py-1.5 px-3 rounded-sm">EN</p>
        <p class="py-1.5 px-3 rounded-sm">THB</p>
-       <p class="py-1.5 px-3 rounded-sm bg-white/80">Search Bookings</p>
-       <p class="bg-white py-1.5 px-3 rounded-sm">Sign in / Register</p>
+       <p class="py-1.5 px-3 rounded-sm bg-white/20">Search Bookings</p>
+       <p class="bg-white/4
+       
+       0 py-1.5 px-3 rounded-sm">Sign in / Register</p>
      </div>
    </div>
    <div class="pb-4 px-10">
      <div
-       class="flex gap-8 text-xl font-light text-black"
+       class="flex gap-8 text-xl font-light text-white"
      >
        <p class="hover:text-black hover:bg-white px-1.5 cursor-pointer">Hotels & Homes</p>
        <p class="hover:text-black hover:bg-white px-1.5 cursor-pointer">Flights</p>
