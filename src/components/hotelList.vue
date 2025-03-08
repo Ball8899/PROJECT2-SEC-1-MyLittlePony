a49253cd5f183f8bb3ca25898a36b1e6c938a7b7
Thammasornn <Thammasorn.chat@mail.kmutt.ac.th>
2025-03-09 00:39:18 +0700

    041 add fucntion listhotel เพื่อใช้ในการloopดึงข้อมูลจากfile .json ตอนนี้อิงเป็นตัวgroupsจากในคลาสเรียนแต่จะมีการupdate file.jsonที่ใช้จริงในการเก็บตัวรูปภาพและรายละเอียดของตัวhotelในภายหลังและใช้slotในการรอรับว่าจะให้ใช้v-forในการloopเอาข้อมูลอะไรออกมา

---
+++ src/components/hotelList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import groups from '../../data/groups.json'
+import listModel from './listModel.vue'
+</script>
+
+<template>
+  <div class="flex">
+    <listModel :items="groups" v-slot="hotelList">
+      
+      <div class="relative flex bg-white w-auto h-auto p-4 mt-5">
+       
+        <img :src="hotelList.hotelDetail.img" alt="Group Image" class="object-cover rounded-md w-40" />
+        
+       
+        <div class="flex flex-col justify-between flex-grow p-2">
+          <div>
+            <span>id: </span>{{ hotelList.hotelDetail.ID }},
+            <span>group name:</span> {{ hotelList.hotelDetail.groupName }}
+          </div>
+          
+          
+          <button class="absolute bottom-2 right-2 bg-blue-500 text-white px-4 py-2 rounded-md">
+            Check In
+          </button>
+        </div>
+      </div>
+    </listModel>
+  </div>
+</template>
+
+  
+
+<style scoped></style>
---
+++ src/components/listModel.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <slot :hotelDetail="item"></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
