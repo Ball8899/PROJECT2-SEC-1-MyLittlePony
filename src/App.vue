138cbf445d1ca6266e605f210e6ba6d1163ea202
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-03-08 18:00:45 +0700

    [027] FIXED resolve import bug

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import Navigation from "./components/Navigation.vue";
-import Header from "./components/Header.vue";
 import Menu from "./components/Menu.vue";
-import ProductManager from "./components/ProductManager.vue";
 import Navbar from "./components/Navbar.vue";
 import MainContent from "./components/MainContent.vue";
 </script>
 
 <template>
   <div>
-    <!-- <div class="bg-cover bg-opacity bg-[100%_65%] bg-[url('https://wallpapercave.com/wp/wp10535399.jpg')]">
-      <Navigation></Navigation>
-      <Header></Header>
-    </div>  -->
     <div>
       <div><Navbar></Navbar></div>
       <div class="flex flex-row">
@@ -20,7 +13,6 @@ import MainContent from "./components/MainContent.vue";
         <MainContent class=""></MainContent>
       </div>
     </div>
-    <!-- <ProductManager></ProductManager>  -->
   </div>
 </template>
 
