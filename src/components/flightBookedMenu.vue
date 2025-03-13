c6cb57848a3dc64cbe381cdf38799250bbe8fb90
Thammasornn <Thammasorn.chat@mail.kmutt.ac.th>
2025-03-13 14:37:21 +0700

    041 add flight booked side bar

---
+++ src/components/flightBookedMenu.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="text-lg w-[290px] text-gray-600 border-r-1 border-gray-200"> 
+    <div class="py-5">
+        <p class="px-8 py-7 flex flex-row gap-6 items-center font-bold">My Bookings</p>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">All</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Flight</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Hotel</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Trains</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Cars</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Package</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Insurance</a></button>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
