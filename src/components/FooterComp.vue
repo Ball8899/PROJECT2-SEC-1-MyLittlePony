b73896aac0b676e6cd5690b1d9ccfa80c6817076
Thammasornn <Thammasorn.chat@mail.kmutt.ac.th>
2025-03-21 03:54:49 +0700

    041 add footer component

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col min-h-60 ">
+    <div class="flex-1">
+    </div>
+    <footer class="bg-gray-100 p-6 mt-auto">
+      <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 text-sm px-50">
+        <div >
+          <h3 class="font-bold mb-2">ติดต่อเรา</h3>
+          <ul>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">บริการลูกค้า</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">การรับประกันบริการ</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ข้อมูลบริการเพิ่มเติม</a></li>
+          </ul>
+          
+        </div>
+
+        <div>
+          <h3 class="font-bold mb-2">เกี่ยวกับ</h3>
+          <ul>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">เกี่ยวกับ Trip.com</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ข่าวประชาสัมพันธ์</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ร่วมงานกับเรา</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ข้อกำหนดและเงื่อนไข</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">นโยบายความเป็นส่วนตัว</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">เกี่ยวกับ Trip.com Group</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="font-bold mb-2">บริการ</h3>
+          <ul>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ที่จองงานสัมมนา</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">โปรแกรม Trip.com Rewards</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">โปรแกรม Trip.com Affiliate Partner</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ลงทะเบียนที่พัก</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">สมัครซัพพลายเออร์ทัวร์และตัวท่องเที่ยว</a></li>
+            <li><a href="#" class="text-gray-600 hover:text-gray-900">ความปลอดภัย</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
