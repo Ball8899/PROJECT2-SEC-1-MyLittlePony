c38330c94220dd4df4e7e25dc82d8007d27d0264
Thammasornn <Thammasorn.chat@mail.kmutt.ac.th>
2025-04-06 03:33:15 +0700

    041 decoration img and font

--- src/components/flightBookedContent.vue
+++ src/components/flightBookedContent.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { getItems, getItemById,deleteItemById } from "../utils/fetchUtil";
+import { getItems, getItemById, deleteItemById } from "../utils/fetchUtil";
 import filterFlightBooked from "./filterFlightBooked.vue";
 import FooterComp from "./FooterComp.vue";
 import flightBookedMenu from "./flightBookedMenu.vue";
 import { storeToRefs } from "pinia";
-import { useBooking } from '../store/booking.js'
+import { useBooking } from "../store/booking.js";
 import { useRouter } from "vue-router";
 import { getAirlineLogo } from "@/utils/toolUtil";
 
@@ -13,7 +13,7 @@ const bookingStore = useBooking();
 const { flightsBookings } = storeToRefs(bookingStore);
 const { getBooking } = bookingStore;
 const router = useRouter();
-const bookingId = ref("")
+const bookingId = ref("");
 
 const getFlights = async (flightId, booking) => {
   try {
@@ -21,7 +21,7 @@ const getFlights = async (flightId, booking) => {
       `${import.meta.env.VITE_APP_URL}/flights`,
       flightId
     );
-    bookingId.value = booking.id
+    bookingId.value = booking.id;
     getBooking(flight, booking);
   } catch (error) {
     console.log(error);
@@ -33,7 +33,7 @@ const getFlightsBooking = async () => {
     const bookings = await getItems(
       `${import.meta.env.VITE_APP_URL}/flightBooking`
     );
-    flightsBookings.value = []; 
+    flightsBookings.value = [];
     for (const booking of bookings) {
       await getFlights(booking.flight, booking);
     }
@@ -68,23 +68,24 @@ const deleteCancelledBooking = async (bookingId) => {
 </script>
 
 <template>
-  
-  <div  class="flex flex-row p-6">
+  <div class="flex flex-row p-6">
     <flightBookedMenu class="w-1/4 mr-6" />
-    <div class="flex flex-col bg-white p-6 rounded-lg shadow-md w-3/4">
+    <div class="flex flex-col bg-white p-6 rounded-lg shadow-md w-3/4" >
       <filterFlightBooked :items="flightsBookings" v-slot="{ booked }">
-        
         <div
-          class="border border-gray-200 rounded-lg p-4 mb-4 shadow max-h-178 overflow-y-auto"
+          class="border border-gray-200 rounded-lg p-4 mb-4 shadow max-h-178 overflow-y-auto cursor-pointer"
+          @click="routerTo(booked.id)"
+                
         >
-        
-    
           <div class="flex justify-between items-center border-b pb-2">
             <div>
               <span class="text-sm text-gray-500">Booking ID:</span>
-              <p @click="routerTo(booked.id)" class="text-blue-500 ml-2 cursor-pointer">
-  {{ booked.id }}
-</p>
+              <p
+                @click="routerTo(booked.id)"
+                class="text-blue-500 ml-2 cursor-pointer"
+              >
+                {{ booked.id }}
+              </p>
             </div>
             <span class="text-sm text-gray-500"
               >Booking Date:
@@ -93,7 +94,12 @@ const deleteCancelledBooking = async (bookingId) => {
           </div>
 
           <div class="flex items-center mt-4">
-            <div>
+            <img
+              :src="getAirlineLogo(booked.flight.flightDetails.airline)"
+              alt="Airline Logo"
+              class="w-15 h-15 object-contain mb-2"
+            />
+            <div class="px-5">
               <p class="text-lg font-semibold">
                 {{ booked.flight.departure.airport.city }} ({{
                   booked.flight.departure.airport.code
@@ -102,46 +108,43 @@ const deleteCancelledBooking = async (bookingId) => {
                 }})
               </p>
               <div
-                class="flex items-center space-x-4 text-gray-500 text-sm mt-1"
+                class="flex items-center space-x-4 text-black text-sm mt-1"
               >
                 <span>{{ booked.flight.departure.time }}</span>
-                <span>-</span>
+                <span class="inline-block w-5 h-0.5 bg-black my-auto"></span>
                 <span>{{ booked.flight.arrival.time }}</span>
-                
-                <span class="flex items-center space-x-1">
-    <span class="text-blue-500 font-medium">
-      {{ booked.flight.flightDetails.flightNumber }}
-    </span>
-    <span>{{ booked.flight.flightDetails.airline }}</span>
-    <img
-      :src="getAirlineLogo(booked.flight.flightDetails.airline)"
-      alt="Airline Logo"
-      class="w-15 h-15 object-contain ml-1 "
-    />
-  </span>
+
+                <span class="text-blue-500 font-medium"
+                >{{
+                  booked.flight.flightDetails.flightNumber
+                }}</span>
+                <span>{{ booked.flight.flightDetails.airline }}</span>
               </div>
-              <p class="text-gray-600 mt-2">
+              <p class="text-black mt-2">
                 Passenger: {{ booked.passenger[0].firstName }}
                 {{ booked.passenger[0].lastName }}
               </p>
             </div>
             <div class="ml-auto text-right">
-              <p class="text-xl font-bold text-gray-800">
-                ฿ {{ booked.flight.pricing.basePrice.toLocaleString() }}
-              </p>
-              <button
-                class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
-                v-if="booked.approve != 'Cancelled'"
-              >
-                Payment
-              </button>
-              <button
-                @click="deleteCancelledBooking(booked.id)"
-                class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
-                v-if="booked.approve == 'Cancelled'"
-              >
-                Delete
-              </button>
+              <div class="ml-auto text-right flex flex-col items-end">
+                <p class="text-xl font-bold text-gray-800">
+                  ฿ {{ booked.flight.pricing.basePrice.toLocaleString() }}
+                </p>
+
+                <button
+                  class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+                  v-if="booked.approve != 'Cancelled'"
+                >
+                  Payment
+                </button>
+                <button
+                  @click="deleteCancelledBooking(booked.id)"
+                  class="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+                  v-if="booked.approve == 'Cancelled'"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -149,7 +152,6 @@ const deleteCancelledBooking = async (bookingId) => {
     </div>
   </div>
   <FooterComp></FooterComp>
-  
 </template>
 
 <style scoped></style>
