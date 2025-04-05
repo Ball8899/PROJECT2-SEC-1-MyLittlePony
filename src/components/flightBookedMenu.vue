8ebf980f4ce43d2b4c4ea677041626d99672c1be
Thammasornn <Thammasorn.chat@mail.kmutt.ac.th>
2025-04-05 22:00:58 +0700

    041 add  rounterTo in menu

--- src/components/flightBookedMenu.vue
+++ src/components/flightBookedMenu.vue
@@ -1,4 +1,11 @@
 <script setup>
+import router from '@/router';
+const routerToHotel = () => {
+  router.push({ name: "HotelBookedContent"});
+};
+const rounterToflight = () =>{
+  router.push({name:"flightBookedContent"})
+}
 </script>
 
 <template>
@@ -6,8 +13,8 @@
     <div class="py-5">
         <p class="px-8 py-7 flex flex-row gap-6 items-center font-bold">My Bookings</p>
         <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">All</a></button>
-        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Flight</a></button>
-        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Hotel</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="" @click="rounterToflight">Flight</a></button>
+        <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="" @click="routerToHotel">Hotel</a></button>
         <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Trains</a></button>
         <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Cars</a></button>
         <button class="cursor-pointer w-[100%] hover:py-6 hover:bg-gray-100 duration-300 px-8 py-7 flex flex-row gap-6 items-center hover:text-blue-600"><a href="">Package</a></button>
